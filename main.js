7f1729c6e9d068d69d37055438a1cedb641f556c
Rajan Kanzariya <rajankanzariya1806@gmail.com>
2024-07-30 22:04:52 +0530

    Git Information

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    
+    sections.forEach((section, index) => {
+        setTimeout(() => {
+            section.style.opacity = 1;
+            section.style.transform = 'translateY(0)';
+        }, index * 300);
+    });
+});
